93a4323930ae55ed863e21bfe2ab6d77939f9173
Arun Kumar <arunkmoury@gmail.com>
2018-09-08 23:14:40 +0530

    final code

---
+++ lib/src/app.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
+import 'widgets/image_list.dart';
+
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    
+    var response =
+        await get('https://jsonplaceholder.typicode.com/photos/$counter');
+
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ImageList(images),
+        appBar: AppBar(
+          title: Text("Header"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed:
+              fetchImage, //if used fetchImage() with paraenthesis whenever build method called this function will execute
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/models/image_model.dart
@@ -0,0 +1,21 @@
+class ImageModel{
+  int id;
+  String url;
+  String title;
+
+  ImageModel(this.id, this.url, this.title);
+
+  ImageModel.fromJson(Map<String, dynamic> parsedJson){
+    id = parsedJson['id'];
+    url = parsedJson['url'];
+    title = parsedJson['title'];
+  }
+
+// short form of above
+  // ImageModel.fromJson(Map<String, dynamic> parsedJson)
+  //   : id = parsedJson['id'],
+  //     url = parsedJson['url'],
+  //     title = parsedJson['title'];
+  
+
+}
